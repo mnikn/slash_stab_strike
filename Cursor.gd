e3e7e07ab01f76ebb40dbcdbc87ef4a38b80d6ac
mnikn <329121948@qq.com>
2020-01-30 19:38:25 +0800

    refactor: remove uesless code

--- Cursor.gd
+++ Cursor.gd
@@ -1,11 +1,5 @@
 extends Node
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-# Called when the node enters the scene tree for the first time.
-
 const TILE_SIZE = 16
 const TILE_NUM_X = 30
 const TILE_NUM_Y = 30
--- Main.gd
+++ Main.gd
@@ -3,6 +3,3 @@ extends Node
 func _ready():
     get_node("Cursor").init()
     get_node("Map").init()
-    # var root = get_tree().get_root()
-    # root.connect("size_changed", self, "resize")
-    # OS.set_window_fullscreen(true)
